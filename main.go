1881a0a5774811d6c60a01d636925c4f57a433f4
Arjan van Eersel <arjanvaneersel@Arjans-MacBook-Pro.local>
2017-04-04 16:09:40 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"runtime"
+
+	"github.com/veandco/go-sdl2/sdl"
+	img "github.com/veandco/go-sdl2/sdl_image"
+	ttf "github.com/veandco/go-sdl2/sdl_ttf"
+)
+
+func run() error {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return fmt.Errorf("Error while initializing SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	err = ttf.Init()
+	if err != nil {
+		return fmt.Errorf("Could not initialize TTF: %v", err)
+	}
+	defer ttf.Quit()
+
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("Error while creating window: %v", err)
+	}
+	defer w.Destroy()
+
+	err = drawTitle(r, "Flappy Gopher")
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	time.Sleep(5 * time.Second)
+
+	s, err := newScene(r)
+	if err != nil {
+		return fmt.Errorf("Error while getting new scene: %v", err)
+	}
+	defer s.destroy()
+
+	events := make(chan sdl.Event)
+	errc := s.run(events, r)
+
+	runtime.LockOSThread()
+	for {
+		select {
+		case events <- sdl.WaitEvent():
+		case err := <-errc:
+			return err
+		}
+	}
+}
+
+func drawTitle(r *sdl.Renderer, text string) error {
+	r.Clear()
+
+	f, err := ttf.OpenFont("res/fonts/flappy.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("Error while opening font: %v", err)
+	}
+	defer f.Close()
+
+	s, err := f.RenderUTF8_Solid(text, sdl.Color{R: 255, G: 100, B: 0, A: 255})
+	if err != nil {
+		return fmt.Errorf("Error while rendering title: %v", err)
+	}
+	defer s.Free()
+
+	t, err := r.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("Error while creating texture: %v", err)
+	}
+	defer t.Destroy()
+
+	err = r.Copy(t, nil, nil)
+	if err != nil {
+		return fmt.Errorf("Error while copying texture: %v", err)
+	}
+	defer r.Present()
+
+	return nil
+}
+
+func drawBackground(r *sdl.Renderer) error {
+	r.Clear()
+	t, err := img.LoadTexture(r, "res/imgs/background.png")
+	if err != nil {
+		return fmt.Errorf("Error while rendering background: %v", err)
+	}
+	defer t.Destroy()
+
+	err = r.Copy(t, nil, nil)
+	if err != nil {
+		return fmt.Errorf("Error while copying background: %v", err)
+	}
+
+	r.Present()
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
+	}
+}
---
+++ pipes.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"sync"
+
+	"github.com/veandco/go-sdl2/sdl"
+	img "github.com/veandco/go-sdl2/sdl_image"
+)
+
+type pipes struct {
+	mu sync.RWMutex
+
+	texture *sdl.Texture
+	speed   int32
+
+	pipes []*pipe
+}
+
+type pipe struct {
+	mu sync.RWMutex
+
+	x, width, height int32
+
+	inverted bool
+}
+
+func newPipes(r *sdl.Renderer) (*pipes, error) {
+	t, err := img.LoadTexture(r, "res/imgs/pipe.png")
+	if err != nil {
+		return nil, fmt.Errorf("Error while rendering pipe image: %v", err)
+	}
+
+	ps := &pipes{
+		texture: t,
+		speed:   2,
+	}
+
+	go func() {
+		for {
+			ps.mu.Lock()
+			ps.pipes = append(ps.pipes, newPipe())
+			ps.mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return ps, nil
+}
+
+func (ps *pipes) paint(r *sdl.Renderer) error {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	for _, p := range ps.pipes {
+		if err := p.paint(r, ps.texture); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (ps *pipes) restart() {
+	ps.mu.Lock()
+	ps.mu.Unlock()
+
+	ps.pipes = nil
+}
+
+func (ps *pipes) update() {
+	ps.mu.Lock()
+	ps.mu.Unlock()
+
+	var rem []*pipe
+	for _, p := range ps.pipes {
+		p.update(ps.speed)
+		if p.x+p.width > 0 {
+			rem = append(rem, p)
+		}
+	}
+	ps.pipes = rem
+}
+
+func (ps *pipes) destroy() {
+	ps.mu.Lock()
+	ps.mu.Unlock()
+
+	ps.texture.Destroy()
+}
+
+func (ps *pipes) touch(b *bird) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	for _, p := range ps.pipes {
+		p.touch(b)
+	}
+}
+
+func newPipe() *pipe {
+	return &pipe{
+		x:        800,
+		height:   100 + int32(rand.Intn(300)),
+		width:    50,
+		inverted: rand.Float32() > 0.5,
+	}
+}
+
+func (p *pipe) paint(r *sdl.Renderer, texture *sdl.Texture) error {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	rect := &sdl.Rect{W: p.width, H: p.height, X: p.x, Y: 600 - p.height}
+	flip := sdl.FLIP_NONE
+	if p.inverted {
+		rect.Y = 0
+		flip = sdl.FLIP_VERTICAL
+
+	}
+
+	err := r.CopyEx(texture, nil, rect, 0, nil, flip)
+	if err != nil {
+		return fmt.Errorf("Error while copying background: %v", err)
+	}
+	return nil
+}
+
+func (p *pipe) update(speed int32) {
+	p.mu.Lock()
+	p.mu.Unlock()
+
+	p.x -= speed
+}
+
+func (p *pipe) touch(b *bird) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	b.touch(p)
+}
